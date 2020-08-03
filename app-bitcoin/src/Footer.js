fa86f8066521bd7325b7d217689705accf4a1ff0
Lyana13 <malaya95lyana@gmail.com>
2020-08-03 15:09:26 +0300

    add folders

---
+++ app-bitcoin/src/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const Footer = () => {
+	return (
+		 <div className="App">
+      		<footer className="App-header">
+        	<img src={logo} className="App-logo" alt="logo" />
+      </footer>
+    </div>
+	)
+}
+
+export default Footer;
---
+++ app-bitcoin/src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'
+
+const Header = () => {
+	return (
+		<>
+		<header>
+			<div>hello header</div>
+		</header>
+		</>
+	)
+}
+
+export default Header;
